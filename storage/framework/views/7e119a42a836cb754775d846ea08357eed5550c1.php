f2a23dfb1ba4516e3e7d34d75595029bb5ec67e2
abdothetruk2 <110694967+abdothetruk2@users.noreply.github.com>
2022-12-26 14:29:13 +0200

    Add files via upload

---
+++ storage/framework/views/7e119a42a836cb754775d846ea08357eed5550c1.php
@@ -0,0 +1,371 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="<?php echo e(asset('css/bootstrap.min.css')); ?>">
+
+    <link rel="stylesheet" href="<?php echo e(asset('css/app.css')); ?>">
+
+</head>
+<body>
+
+    <!-- header -->
+    <nav class=" navbar navbar-expand-lg navbar-dark bg-dark">
+
+
+
+
+
+        <a class="justify-content-center text-center navbar-brand" href="#"><img width="50%" src="<?php echo e(asset('img/logo-light.png')); ?>" alt="" srcset=""></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="d-flex justify-content-end   collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">HOME</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">PAGES</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">PORTIFILIO</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="#">BLOG</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="#">SHOP</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link fas fa-search" href="#"></a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link fas fa-shopping-cart" href="#"></a>
+              </li>
+
+              <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Methods
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+
+          </ul>
+          <form   class="d-none form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+
+
+        </form>
+        </div>
+
+    </nav>
+
+    <div class='navnav2'>
+
+
+        <div class='text-center'>
+        <img src='<?php echo e(asset("img/h1.png")); ?>'>
+
+
+
+    <h1> COFFEE FACTORY</h1>
+
+    <img src='<?php echo e(asset("img/sperator.png")); ?>'>
+
+ <div class='text-center d-flex justify-content-center'>    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid idunt ut labore et dolore magna aliqua atin script and its variants, and. </p> </div>
+
+
+   <button class='btn btn-outline-secondary related'>SHOP HERE</button>
+
+  </div>
+
+
+    </div>
+    <div class='navnav mb-5' >
+
+
+
+
+
+    </div>
+
+
+    <!-- header -->
+<!-- body -->
+
+<div class='container'>
+
+<div class='text-center mt-5 p-5 '>
+<h1>   OUR DELECIOUS OFFER     </h1>
+<img src='<?php echo e(asset("img/title-separator.png")); ?>'>
+<p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+    <p>
+
+
+
+
+
+
+</div>
+
+
+<div class='row row-cols-lg-4 row-cols-md-2 mb-5'>
+
+
+    <div class='text-center'> <img src=<?php echo e(asset("img/h2-custom-icon-5.png")); ?>> <h1> Types OF Coffee </h1> <p>Lorem ipsum dolor sit ametal, consectetuer adipiscing elitus. Aeneantos commodo  <p> </div>
+
+        <div class='text-center'> <img src=<?php echo e(asset("img/h2-custom-icon-6.png")); ?>> <h1> Bean Varieties </h1> <p>Lorem ipsum dolor sit ametal, consectetuer adipiscing elitus. Aeneantos commodo  <p> </div>
+
+
+
+
+
+
+    <div class='text-center'> <img src=<?php echo e(asset("img/h2-custom-icon-7.png")); ?>> <h1> Types OF Coffee </h1> <p>Lorem ipsum dolor sit ametal, consectetuer adipiscing elitus. Aeneantos commodo  <p> </div>
+
+        <div class='text-center'> <img src=<?php echo e(asset("img/h2-custom-icon-8.png")); ?>> <h1> Bean Varieties </h1> <p>Lorem ipsum dolor sit ametal, consectetuer adipiscing elitus. Aeneantos commodo  <p> </div>
+
+
+</div>
+
+
+
+
+
+
+</div>
+
+
+
+
+
+<div class='section2 p-5'>
+
+<div class='container'>
+
+
+    <div class='text-center mb-5'>
+
+        <h1>  THE COFFEE HEALED </h1>
+         <img  src=<?php echo e(asset("img/title-separator.png")); ?>>
+         <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+            </p>
+
+  </div>
+
+
+
+
+
+
+  <div class=' row row-cols-lg-4  justify-content-center '>
+
+    <div>  <img width="80%"  src=<?php echo e(asset("img/blog-1-img-1-800x646.jpg")); ?>>    <h3>Prep Techniques </h3>  <p style="margin:0 !important;">Quisque rutrum, aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi, nam eget dui. Etiam rhoncus maecenas tempus,  </p>                  </div>
+    <div>  <img width="80%" src=<?php echo e(asset("img/blog-1-img-2-800x646.jpg")); ?>>    <h3>French Press</h3>  <p style="margin:0 !important;">Quisque rutrum, aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi, nam eget dui. Etiam rhoncus maecenas tempus,  </p>                  </div>
+    <div>  <img width="80%" src=<?php echo e(asset("img/blog-1-img-3-800x646.jpg")); ?>>    <h3>turkish_Ibrik </h3>  <p style="margin:0 !important;">Quisque rutrum, aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi, nam eget dui. Etiam rhoncus maecenas tempus,  </p>                  </div>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+</div>
+
+
+
+
+
+</div>
+
+<div class='navnav3 mb-5'>
+
+
+    <img src=<?php echo e(asset("img/logo2.png")); ?>>
+
+
+</div>
+<div class='container mt-5 mb-5'>
+
+    <div class='row row-cols-lg-4'>
+<div class='text-center'> <h1 style="color:#be9c79">   250</h1> <h3 style="font-family:Marcellus">  Varieties of Coffee  </h3> <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit aenean </p> </div>
+<div class='text-center'> <h1 style="color:#be9c79">   123</h1> <h3 style="font-family:Marcellus">  Hours of Testing  </h3> <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit aenean </p> </div>
+<div class='text-center'> <h1 style="color:#be9c79">   321</h1> <h3 style="font-family:Marcellus">   Coffee Markets   </h3> <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit aenean </p> </div>
+<div class='text-center'> <h1 style="color:#be9c79">   220</h1> <h3 style="font-family:Marcellus">   Coffee Brands  </h3> <p> Lorem ipsum dolor sit amet, consectetuer adipiscing elit aenean </p> </div>
+
+
+    </div>
+
+
+</div>
+
+
+<div  class='section3 p-5'>
+
+<div class='container mt-5 '>
+
+
+    <div class='text-center mb-5 mt-5'>
+
+        <h1>
+            Online Coffee Shop
+            </h1>
+         <img  src=<?php echo e(asset("img/title-separator.png")); ?>>
+         <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+            </p>
+
+  </div>
+
+
+
+
+  <div class='row row-cols-lg-4'>
+    <div class='text-center'> <img width=70% src="<?php echo e(asset('img/product-img-1-550x550.png')); ?>" alt="" srcset=""> <h3 style="font-family:Marcellus">  Ethopia Coffee </h3> <p> 15$ </p> </div>
+    <div class='text-center'> <img width=70% src="<?php echo e(asset('img/product-img-3-550x550.png')); ?>" alt="" srcset=""> <h3 style="font-family:Marcellus">  Keny Coffee </h3> <p> 35$ </p> </div>
+    <div class='text-center'> <img width=70% src="<?php echo e(asset('img/product-img-5-550x550.png')); ?>" alt="" srcset=""> <h3 style="font-family:Marcellus">  Columbia Coffee </h3> <p> 55$ </p> </div>
+    <div class='text-center'> <img width=70% src="<?php echo e(asset('img/product-img-8-550x550.png')); ?>" alt="" srcset=""> <h3 style="font-family:Marcellus">  Guatimale Coffee </h3> <p> 20$ </p> </div>
+
+
+        </div>
+
+
+
+</div>
+
+
+
+</div>
+
+
+<div  class='container mt-5 mb-5'>
+
+    <div class='text-center mb-5 mt-5'>
+
+        <h1>
+
+            Our Sweet Gallery
+        </h1>
+         <img  src=<?php echo e(asset("img/title-separator.png")); ?>>
+         <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+            </p>
+
+  </div>
+
+</div>
+
+<div class='gallrey row row-cols-lg-4 gy-4'>
+
+<img width="25%" src=<?php echo e(asset("img/port-1-gallery-1.jpg")); ?>>
+
+<img width="25%" src=<?php echo e(asset("img/port-1-gallery-3.jpg")); ?>>
+<img width="25%" src=<?php echo e(asset("img/port-1-gallery-4.jpg")); ?>>
+<img width="25%" src=<?php echo e(asset("img/port-1-gallery-6.jpg")); ?>>
+<img width="25%" src=<?php echo e(asset("img/port-1-gallery-7.jpg")); ?>>
+<img width="25%" src=<?php echo e(asset("img/port-1-gallery-8.jpg")); ?>>
+<img width="25%" src=<?php echo e(asset("img/port-1-gallery-9.jpg")); ?>>
+<img width="25%" src=<?php echo e(asset("img/port-1-gallery-10.jpg")); ?>>
+
+
+
+
+
+</div>
+
+<br>
+<br>
+
+<div class='container mt-5 mb-5'>
+
+
+    <img width="15%" src=<?php echo e(asset("img/clients-img-1.png")); ?>>
+    <img width="15%" src=<?php echo e(asset("img/clients-img-2-hover.png")); ?>>
+    <img width="15%" src=<?php echo e(asset("img/clients-img-3-hover.png")); ?>>
+    <img width="15%" src=<?php echo e(asset("img/clients-img-4-hover.png")); ?>>
+    <img width="15%" src=<?php echo e(asset("img/clients-img-5-hover.png")); ?>>
+    <img width="15%" src=<?php echo e(asset("img/clients-img-6-hover.png")); ?>>
+
+
+
+</div>
+
+
+
+
+    <div style="padding:40px; background-image: url(<?php echo e(asset('img/landing-bakcground-img-1.jpg')); ?>)">
+        <div class='container'>
+
+
+            <div class='row row-cols-2'>
+
+            <h4>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+         </h4>
+
+         <button  class='btn btn-outline-secondary'>CONTACT US </button>
+
+        </div>
+</div>
+</div>
+
+
+<!-- -->
+
+
+<footer >
+
+    <div class='container'>
+
+        <div class='footer row row-cols-lg-3'>
+
+<div>© 2018 Qode Interactive, All Rights Reserved </div>
+
+<div class='justify-content-between d-flex'> <a style="color:white; text-decoration:none font-family:'Marcellus' " > HOME</a>  <a style="color:white; text-decoration:none font-family:'Marcellus' " > ABOUT</a> <a style="color:white; text-decoration:none font-family:'Marcellus' " > SHOP</a>  <a style="color:white; text-decoration:none font-family:'Marcellus' " > CONTACT</a>  </div>
+
+
+<div  class='col-lg-2 d-flex text-end  justify-content-between '><i class="fab fa-instagram-square"></i> <i class="fab fa-twitter"></i><i class="fab fa-facebook-f"></i> <i class="fab fa-tumblr"></i> <i class="fab fa-vimeo-v"></i> </div>
+
+
+</div>
+
+
+</div>
+
+</footer>
+
+
+
+
+    <script src="<?php echo e(asset('js/app.js')); ?>"> </script>
+    <script src="<?php echo e(URL('https://kit.fontawesome.com/9e8a4a692d.js ')); ?>"></script>
+    <script src="<?php echo e(asset('js/bootstrap.min.js')); ?>"> </script>
+
+    <script src="<?php echo e(asset('js/jquery-3.6.1.min.js')); ?>"> </script>
+</body>
+</html>
+<?php /**PATH D:\app\resources\views/index.blade.php ENDPATH**/ ?>
